3dd07bc50bde8bfa3aa2a9db90d5e5c23cc6dd0e
frankwoodtiger <chil@clemson.edu>
2015-09-30 18:23:01 -0400

    Added more samples

--- Trees/BinaryTree.py
+++ Trees/BinaryTree.py
@@ -1,12 +1,56 @@
-import Node
+from Node import Node 
 
-class BinaryTree:
+class BinaryTree(object):
 	"""Binary tree class"""
-	def __init__(self, rootLoad)
-		self.root = Node(rootLoad)
+	def __init__(self, rootNode):
+		self._root = rootNode
 		
+	def getRoot(self):
+		return self._root
+	
 	def insert(self, node):
-		tempNode = root
+		tempNode = self._root
+		
 		while tempNode is not None:
-			if tempNode.getLoad
-			
+			parentNode = tempNode
+			if node.load <= tempNode.load:
+				tempNode = tempNode.leftNode
+				if tempNode is None:
+					parentNode.leftNode = node
+					break
+			else:
+				tempNode = tempNode.rightNode
+				if tempNode is None:
+					parentNode.rightNode = node
+					break
+				
+	def _preOrderTransversal_helper(self, root):
+		if root is None:
+			return None
+		
+		leftLoad = self._preOrderTransversal_helper(root.leftNode)
+		leftLoad = " -> " +  leftLoad if leftLoad is not None else ""
+		rightLoad = self._preOrderTransversal_helper(root.rightNode)
+		rightLoad = " -> " +  rightLoad if rightLoad is not None else ""
+		return root.load + leftLoad + rightLoad
+		 
+	
+	def preOrderTransversal(self):
+		return self._preOrderTransversal_helper(self._root)
+		
+if __name__=="__main__":
+	btree = BinaryTree(Node('F'));
+	btree.insert(Node('B'));
+	btree.insert(Node('G'));
+	btree.insert(Node('A'));
+	btree.insert(Node('D'));
+	btree.insert(Node('C'));
+	btree.insert(Node('E'));
+	btree.insert(Node('I'));
+	btree.insert(Node('H'));
+	
+	root = btree.getRoot()
+	print root.leftNode
+	print root.rightNode
+	
+	print btree.preOrderTransversal()
--- Trees/Node.py
+++ Trees/Node.py
@@ -1,25 +1,58 @@
 
-class Node:
+class Node(object):
 	"""Node class serves the basis of tree node"""
-	def __init__(self, load=0, leftNode=None, rightNode=None)
-		self.load=load
-		self.leftNode=leftNode
-		self.rightNode=rightNode
-		
-	def setLoad(self, load)
-		self.load = load
+	numOfNode = 0;
+	def __init__(self, load=0, leftNode=None, rightNode=None):
+		self._load=load
+		self._leftNode=leftNode
+		self._rightNode=rightNode
+		self._numOfNode = self.numOfNode + 1
 		
 	def getLoad(self):
-		return self.load
-		
-	def setLeftNode(self, leftNode):
-		self.leftNode = leftNode
-		
+		return self._load
+
+	def setLoad(self, load):
+		self._load = load
+	
+	load = property(getLoad, setLoad)
+	
 	def getLeftNode(self):
-		return self.leftNode
+		return self._leftNode
+	
+	def setLeftNode(self, leftNode):
+		self._leftNode = leftNode
 		
-	def setRightNode(self, rightNode):
-		self.rightNode = rightNode
-		
+	leftNode = property(getLeftNode, setLeftNode)
+	
 	def getRightNode(self):
-		return self.rightNode
+		return self._rightNode
+	
+	def setRightNode(self, rightNode):
+		self._rightNode = rightNode
+	
+	rightNode = property(getRightNode, setRightNode)
+	
+	def __str__(self):
+		return self._load
+	
+# Test for Node class
+if __name__=="__main__":
+	rootNode = Node("root")
+	leftNode = Node("left")
+	rightNode = Node("right")
+	rootNode.leftNode = leftNode
+	rootNode.rightNode = rightNode
+	
+	# normal test case
+	print type(rootNode)
+	print rootNode
+	print rootNode.load
+	
+	print rootNode.leftNode
+	print rootNode.rightNode
+
+	# null test case
+	print rootNode.leftNode.leftNode
+	print Node.numOfNode
+	
+	
